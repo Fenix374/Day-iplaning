1d1dbc66f927cdb24222c5afbb200967f47446f0
Fenix374 <isip_s.g.lysenko@mpt.ru>
2024-02-27 22:18:49 +0300

    WpfApp5

---
+++ WpfApp5/Crud.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DailyPlanner
+{
+    public class Crud
+    {
+        private string filePath;
+
+        public Crud(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public void Serialize(List<Note> notes)
+        {
+            string json = JsonConvert.SerializeObject(notes);
+            File.WriteAllText(filePath, json);
+        }
+
+        public List<Note> Deserialize()
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<List<Note>>(json);
+            }
+            else
+            {
+                return new List<Note>();
+            }
+        }
+    }
+}
---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace DailyPlanner
+{
+    public partial class MainWindow : Window
+    {
+        private Crud crUd;
+        private List<Note> notes = new List<Note>();
+        private DateTime selectedDate;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            crUd = new Crud("C:\\Users\\semen\\source\\repos\\WpfApp5\\WpfApp5\\dailyplaining.json");
+            LoadNotes();
+            selectedDate = DateTime.Today;
+            DisplayNotes(selectedDate);
+        }
+
+        private void LoadNotes()
+        {
+            notes = crUd.Deserialize();
+        }
+
+        private void SaveNotes()
+        {
+            crUd.Serialize(notes);
+        }
+
+        private void DisplayNotes(DateTime date)
+        {
+            notesList.ItemsSource = null;
+            notesList.ItemsSource = notes.FindAll(note => note.Date == date);
+        }
+
+        private void Calendar_SelectedDatesChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            selectedDate = calendar.SelectedDate ?? DateTime.Today;
+            DisplayNotes(selectedDate);
+        }
+
+        private void NotesList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (notesList.SelectedItem != null)
+            {
+                Note selectedNote = (Note)notesList.SelectedItem;
+                noteTitleTextBox.Text = selectedNote.Title;
+                noteDescriptionTextBox.Text = selectedNote.Description;
+            }
+        }
+
+        private void AddNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            Note newNote = new Note
+            {
+                Title = noteTitleTextBox.Text,
+                Description = noteDescriptionTextBox.Text,
+                Date = selectedDate
+            };
+            notes.Add(newNote);
+            SaveNotes();
+            DisplayNotes(selectedDate);
+        }
+
+        private void UpdateNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (notesList.SelectedItem != null)
+            {
+                Note selectedNote = (Note)notesList.SelectedItem;
+                selectedNote.Title = noteTitleTextBox.Text;
+                selectedNote.Description = noteDescriptionTextBox.Text;
+                SaveNotes();
+                DisplayNotes(selectedDate);
+            }
+        }
+
+        private void DeleteNoteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (notesList.SelectedItem != null)
+            {
+                Note selectedNote = (Note)notesList.SelectedItem;
+                notes.Remove(selectedNote);
+                SaveNotes();
+                DisplayNotes(selectedDate);
+            }
+        }
+    }
+
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
